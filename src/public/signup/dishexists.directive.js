7949814481174395f2e99f5dd4ea9db553f88e60
Shemp <shemplab@gmail.com>
2024-05-07 22:43:12 -0400

    working async validation

---
+++ src/public/signup/dishexists.directive.js
@@ -0,0 +1,43 @@
+(function () {
+    'use strict';
+
+    angular.module('public')
+        .directive('dishExists', dishExists);
+
+    dishExists.$inject = ['$http', '$q', 'ApiPath']
+    function dishExists($http, $q, ApiPath) {
+        return {
+            require: 'ngModel',
+            link: function (scope, elm, attrs, ngModel) {
+
+                elm.on('keyup', function () {
+                    ngModel.$asyncValidators.dishExists = function (modelValue, viewValue) {
+
+                        var itemNumberString = modelValue.match(/[0-9]+/);
+                        var itemCategory = modelValue.match(/^[a-zA-Z]+/);
+                        var itemNumber = itemNumberString - 1;
+
+                        return $http.get(ApiPath + `/menu_items/${itemCategory[0].toUpperCase()}/menu_items/${itemNumber}.json`)
+                            .then(
+                                function (response) {
+                                    if (response.data === null) {
+                                        return $q.reject('menu item doesnt exist');
+                                    }
+                                    else {
+                                        return true;
+                                    }
+                                }
+                            )
+                    }
+                })
+
+
+
+
+
+            }
+
+        }
+    }
+
+})();
